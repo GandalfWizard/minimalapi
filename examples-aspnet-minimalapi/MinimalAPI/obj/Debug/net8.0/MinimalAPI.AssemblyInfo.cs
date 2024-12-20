cf792508d6a19cad4b3cc783c2518de89fcf7ff0
De Wolf Remco <remco.de.wolf@outlook.com>
2024-12-20 20:00:36 +0100

    merge

--- examples-aspnet-minimalapi/MinimalAPI/obj/Debug/net8.0/MinimalAPI.AssemblyInfo.cs
+++ examples-aspnet-minimalapi/MinimalAPI/obj/Debug/net8.0/MinimalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9aaa56f5b7afd87a83d6e8932ff7086283e41289")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
