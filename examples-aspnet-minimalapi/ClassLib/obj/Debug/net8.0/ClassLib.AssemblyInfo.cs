ed1885881e6e8981faeee7e6972af8853dd56e1a
De Wolf Remco <remco.de.wolf@outlook.com>
2024-12-21 03:01:34 +0100

    fix bug

--- examples-aspnet-minimalapi/ClassLib/obj/Debug/net8.0/ClassLib.AssemblyInfo.cs
+++ examples-aspnet-minimalapi/ClassLib/obj/Debug/net8.0/ClassLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f2c3890f6e91a8908fc8330bdff956d6a28160")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6773fde964faadd0e1a7f1a6e1a12bf23cee3593")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
